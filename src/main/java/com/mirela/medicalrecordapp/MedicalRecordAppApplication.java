54dbe05f79e5aadd60b67b4a13cf7f53607a6d52
Mirela <mirelaboncheva4@gmail.com>
2024-10-30 17:31:37 +0200

    Initial commit

---
+++ src/main/java/com/mirela/medicalrecordapp/MedicalRecordAppApplication.java
@@ -0,0 +1,13 @@
+package com.mirela.medicalrecordapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalRecordAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalRecordAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mirela/medicalrecordapp/MedicalRecordAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mirela.medicalrecordapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalRecordAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
