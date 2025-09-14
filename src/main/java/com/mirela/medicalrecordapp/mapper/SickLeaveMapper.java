7213a73b317f4dc99b20a7a49a24ec3c19f8bff6
Mirela <mirelaboncheva4@gmail.com>
2025-09-14 12:58:25 +0300

    feat: Implement full CRUD admin Manage SickLeaves backend

--- src/main/java/com/mirela/medicalrecordapp/mapper/SickLeaveMapper.java
+++ src/main/java/com/mirela/medicalrecordapp/mapper/SickLeaveMapper.java
@@ -1,8 +1,10 @@
 package com.mirela.medicalrecordapp.mapper;
 
 import com.mirela.medicalrecordapp.dto.SickLeaveDTO;
+import com.mirela.medicalrecordapp.dto.admin.SickLeaveDto;
 import com.mirela.medicalrecordapp.model.SickLeave;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 import java.util.List;
@@ -14,4 +16,15 @@ public interface SickLeaveMapper {
 
     SickLeaveDTO toDTO(SickLeave sickLeave);
     List<SickLeaveDTO> toDTOList(List<SickLeave> sickLeaves);
+
+    @Mapping(target = "sickLeaveId", source = "id")
+    @Mapping(target = "appointmentId", source = "appointment.id")
+    @Mapping(target = "patientId", source = "appointment.patient.id")
+    @Mapping(target = "patientName",
+            expression = "java(s.getAppointment().getPatient().getUser().getFirstName() + \" \" + s.getAppointment().getPatient().getUser().getLastName())")
+    @Mapping(target = "doctorId", source = "appointment.doctor.id")
+    @Mapping(target = "doctorName",
+            expression = "java(s.getAppointment().getDoctor().getUser().getFirstName() + \" \" + s.getAppointment().getDoctor().getUser().getLastName())")
+    @Mapping(target = "specialization", source = "appointment.doctor.specialization")
+    SickLeaveDto toDto(SickLeave s);
 }
--- src/main/java/com/mirela/medicalrecordapp/repository/SickLeaveRepository.java
+++ src/main/java/com/mirela/medicalrecordapp/repository/SickLeaveRepository.java
@@ -1,7 +1,13 @@
 package com.mirela.medicalrecordapp.repository;
 
 import com.mirela.medicalrecordapp.model.SickLeave;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface SickLeaveRepository extends JpaRepository<SickLeave, Long> {
+
+    @EntityGraph(attributePaths = {"appointment.patient.user", "appointment.doctor.user"})
+    List<SickLeave> findAll();
 }
